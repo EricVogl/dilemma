9c48adcb67346bcbe5910f70e093eabc27eaadfe
Eric Vogl <ericvogl@gmail.com>
2014-04-19 02:53:47 -0400

    Forgot to add a file in last commit

--- vogl.rb
+++ vogl.rb
@@ -1,14 +1,27 @@
 #!/usr/bin/ruby
 
-require "./processor.rb"
-require "./strategy.rb"
-require "./random_strategy.rb"
+require_relative "processor"
+require_relative "strategy"
+require_relative "random_strategy"
+require_relative "sabotage_strategy"
+require_relative "cooperate_strategy"
 
 opponentName = ""
 if (ARGV.length > 0)
   opponentName = ARGV[0].chomp
 end
 
-strategy = RandomStrategy.new(opponentName)
+strategy = Strategy.new(opponentName)
+if (ARGV.length > 1)
+  case (ARGV[1])
+    when "sabotage"
+      strategy = SabotageStrategy.new(opponentName)
+    when "coop"
+      strategy = CooperateStrategy.new(opponentName)
+    when "random"
+      strategy = RandomStrategy.new(opponentName)
+  end
+end
+
 processor = Processor.new(strategy)
 processor.process
