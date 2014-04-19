6f8d2a08113897cdfb503b8b3e8c9f17b009c827
Eric Vogl <ericvogl@gmail.com>
2014-04-19 02:17:21 -0400

    Added basic functionality

--- vogl.rb
+++ vogl.rb
@@ -1,3 +1,57 @@
 #!/usr/bin/ruby
 
-puts "Hello world"
+class Strategy
+  def initialize(opponentName)
+    @opponentName = opponentName
+  end
+
+  def doTurn
+    return "C"
+  end
+
+  def opponentCooperate
+  end
+
+  def opponentSabotage
+  end
+end
+
+class Processor
+  def initialize(strategy)
+    @strategy = strategy
+  end
+
+  def process
+    until ((input = STDIN.gets.chomp) == "quit")
+      if (input == "turn")
+        processTurn
+      elsif
+        raise "Unexpected input: '#{input}'"
+      end
+    end
+  end
+
+  private
+  def processTurn
+    turn = @strategy.doTurn
+    STDOUT.puts turn
+    input = STDIN.gets.chomp
+
+    if (input == "C")
+      @strategy.opponentCooperate
+    elsif (input == "S")
+      @strategy.opponentSabotage
+    elsif
+      raise "Expected enemy turn.  Unexpected input: '#{input}'"
+    end
+  end
+end
+
+opponentName = ""
+if (ARGV.length > 0)
+  opponentName = ARGV[0].chomp
+end
+
+strategy = Strategy.new(opponentName)
+processor = Processor.new(strategy)
+processor.process
